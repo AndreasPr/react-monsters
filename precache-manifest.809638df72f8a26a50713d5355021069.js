15e30198e808da5e3ffcd953707fc1130a45b7fa
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-04-16 20:39:39 -0400

    Updates

---
+++ precache-manifest.809638df72f8a26a50713d5355021069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78aefd2f7575b0e225cc0926405ddd38",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "7320631aa8b6d6c196ee",
+    "url": "/react-monsters/static/css/main.008effa6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7320631aa8b6d6c196ee",
+    "url": "/react-monsters/static/js/main.42ff86af.chunk.js"
+  },
+  {
+    "revision": "9e48f53adff59196fe9e",
+    "url": "/react-monsters/static/js/runtime-main.265b368b.js"
+  }
+]);
